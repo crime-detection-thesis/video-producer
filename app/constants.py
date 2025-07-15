d8b38091803bf919e3cfb0f259aee5325e4abe23
JorgeIP10 <jorgep.ipanaquep@gmail.com>
2025-07-15 16:35:09 -0500

    feat: change logic to send detections to the backend

--- app/constants.py
+++ app/constants.py
@@ -4,3 +4,6 @@ import os
 load_dotenv()
 
 INFERENCE_SERVER_URL = os.getenv("INFERENCE_SERVER_URL")
+INCIDENT_WINDOW = 10
+UPLOAD_SECRET = os.getenv("UPLOAD_SECRET", "").encode()
+UPLOAD_URL = os.getenv("UPLOAD_URL")
